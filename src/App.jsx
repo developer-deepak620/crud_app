ec33131b1962ca24371dd75bdefcfa410b2a94ec
Deepak Kumar <kumardeepak64461@gmail.com>
2025-11-18 17:32:11 +0530

    crud3

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
   }
 
   return (
-    <div className='container py-4 overflow-y-hidden'>
+    <div className='container py-4'>
       {userData == null ?
 
         <div className="d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
